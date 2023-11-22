9ad0147eeabd8075c331172a81e6be363360c025
Evan Carlson <evanrc1@gmail.com>
2023-11-22 14:15:03 -0800

    added package.json and installed inquirer. Also added code to require inquirer and fs in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs'); //for file system operations
 // TODO: Create an array of questions for user input
 const questions = [];
 
